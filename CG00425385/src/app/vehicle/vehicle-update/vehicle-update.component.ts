d474cd21de9a52a81184a5cbbe7179a867efeb90
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-04-28 00:47:14 +0000

    Updated layouts.

--- CG00425385/src/app/vehicle/vehicle-update/vehicle-update.component.ts
+++ CG00425385/src/app/vehicle/vehicle-update/vehicle-update.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Vehicle, VehicleService } from '../vehicle.service';
 
 @Component({
   selector: 'app-vehicle-update',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './vehicle-update.component.html',
   styleUrls: ['./vehicle-update.component.css'],
 })
@@ -15,6 +15,7 @@ export class VehicleUpdateComponent implements OnInit {
   vehicle: Vehicle | null = null;
   newMid: string = '';
   loading: boolean = true;
+  errorMessage: string = '';
 
   constructor(
     private vehicleService: VehicleService,
@@ -25,7 +26,7 @@ export class VehicleUpdateComponent implements OnInit {
   ngOnInit(): void {
     // First, check if we have a reg parameter in the URL
     const reg = this.route.snapshot.paramMap.get('reg');
-    
+
     if (reg) {
       this.loadVehicle(reg);
     } else {
@@ -35,11 +36,11 @@ export class VehicleUpdateComponent implements OnInit {
         this.loadVehicle(selectedReg);
       } else {
         // Navigate to error page if no registration provided
-        this.router.navigate(['/error'], { 
-          queryParams: { 
+        this.router.navigate(['/error'], {
+          queryParams: {
             message: 'No vehicle registration provided',
-            status: 400
-          }
+            status: 400,
+          },
         });
       }
     }
@@ -53,7 +54,7 @@ export class VehicleUpdateComponent implements OnInit {
           this.newMid = vehicle.mechanic.mid;
         }
         this.loading = false;
-      }
+      },
       // Error handling is now done in the service
     });
   }
@@ -62,7 +63,7 @@ export class VehicleUpdateComponent implements OnInit {
     if (!this.vehicle) return;
 
     this.loading = true;
-    
+
     this.vehicleService
       .updateVehicleMechanic(this.vehicle.reg, this.newMid)
       .subscribe({
